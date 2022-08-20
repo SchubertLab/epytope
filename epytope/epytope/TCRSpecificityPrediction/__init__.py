974e05216f2256d8cfd1339932b7be00d545342c
MahmoudAlbahah <mahmoudalbahah@gmail.com>
2022-08-20 02:47:11 +0200

    TCR specificity prediction was implemented

---
+++ epytope/epytope/TCRSpecificityPrediction/__init__.py
@@ -0,0 +1,45 @@
+from epytope.Core.Base import ATCRSpecificityPrediction
+from epytope.TCRSpecificityPrediction.External import *
+
+try:
+    from fred_plugin import *
+except ImportError:
+    pass
+
+
+class MetaclassTCRSpecificity(type):
+    def __init__(cls, name, bases, nmspc):
+        type.__init__(cls, name, bases, nmspc)
+
+    def __call__(self, _predictor, *args, **kwargs):
+        """
+        If a third person wants to write a new Cleavage Site Predictor. One has to name the file fred_plugin and
+        inherit from ACleavagePrediction. That's it nothing more.
+        """
+
+        version = str(kwargs["version"]).lower() if "version" in kwargs else None
+        try:
+            return ATCRSpecificityPrediction[str(_predictor).lower(), version](*args)
+        except KeyError as e:
+            if version is None:
+                raise ValueError(
+                    "Predictor %s is not known. Please verify that such an Predictor is " % _predictor +
+                    "supported by epytope and inherits ACleavageSitePrediction.")
+            else:
+                raise ValueError(
+                    "Predictor %s version %s is not known. Please verify that such an Predictor is " % (
+                        _predictor, version) +
+                    "supported by epytope and inherits ACleavageSitePrediction.")
+
+
+class TCRSpecificityPredictorFactory(metaclass=MetaclassTCRSpecificity):
+
+    @staticmethod
+    def available_methods():
+        """
+        Returns a list of available cleavage site predictors
+        :return: dict(str,list(int)) - dict of cleavage site predictor represented as string and the supported versions
+        """
+        return {k: sorted(versions.keys()) for k, versions in ATCRSpecificityPrediction.registry.items()}
+
+
---
+++ epytope/epytope/test/TestAntigenImmuneReceptor.py
@@ -0,0 +1,36 @@
+"""
+Unit test for AntigenImmuneReceptor class
+"""
+
+import unittest
+
+from epytope.Core.AntigenImmuneReceptor import AntigenImmuneReceptor, BCellReceptor, TCellReceptor
+from epytope.Core.ImmuneReceptorChain import ImmuneReceptorChain
+
+
+class TestImmuneReceptorChainClass(unittest.TestCase):
+    def setUp(self):
+        alpha = ImmuneReceptorChain(chain_id="57", chain_type="TRA", v_gene="TRAV26-1", d_gene=None,
+                                         j_gene="TRAJ37", cdr3="IVVRSSNTGKLI", cdr1="TISGNEY", cdr2="GLKNN")
+        beta = ImmuneReceptorChain(chain_id="57", chain_type="TRB", v_gene="TRBV14", d_gene=None,
+                                         j_gene="TRBJ2-3", cdr3="ASSQDRDTQY", cdr1="SGHDN", cdr2="FVKESK")
+        self.tcr = AntigenImmuneReceptor(receptor_id=alpha.chain_id, chains=[alpha, beta], cell_type="CD8")
+
+        heavy_chain = ImmuneReceptorChain(chain_id="58", chain_type="IGH", v_gene="", d_gene=None,
+                                         j_gene="", cdr3="IVVRSSNTGKLI")
+        light_chain = ImmuneReceptorChain(chain_id="58", chain_type="IGL", v_gene="", d_gene=None,
+                                          j_gene="", cdr3="IVVRSSNTGKLI")
+        self.bcr = AntigenImmuneReceptor(receptor_id=alpha.chain_id, chains=[heavy_chain, light_chain], cell_type="",
+                                         t_cell=False)
+
+    def test1_chain_construction_novariants(self):
+        self.assertEqual(self.tcr.receptor_id, "57", 'incorrect Id')
+        self.assertEqual(self.tcr.cell_type, "CD8", 'incorrect chain type')
+        self.assertIsInstance(self.tcr.chains[0], ImmuneReceptorChain, "incorrect chain type")
+        self.assertIsInstance(self.tcr.chains[1], ImmuneReceptorChain, "incorrect chain type")
+        self.assertIsInstance(self.tcr, TCellReceptor, "incorrect receptor type")
+        self.assertIsInstance(self.bcr, BCellReceptor, "incorrect receptor type")
+
+
+if __name__ == '__main__':
+    unittest.main()
