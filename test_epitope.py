8ff3b3610161aaca9e3b604a047118a87fa6ef76
annachernysheva179 <anna.chernysheva@tum.de>
2023-08-17 08:30:20 +0200

    debug

---
+++ test_epitope.py
@@ -0,0 +1,24 @@
+import sys
+sys.path.append('/home/icb/anna.chernysheva/epytope')
+
+from epytope.Core import Peptide, Allele
+from epytope.Core import Peptide, Allele, TCREpitope, ImmuneReceptorChain, ImmuneReceptor
+from epytope.IO import IRDatasetAdapterFactory
+from epytope.TCRSpecificityPrediction import TCRSpecificityPredictorFactory
+import pandas as pd
+
+
+peptide = Peptide("SYFPEITHI")
+allele = Allele("HLA-A*02:01")
+epitope_1 = TCREpitope(peptide=peptide, allele=allele)
+
+epitope_2 = TCREpitope(peptide="EAAGIGILTV", allele=None)
+
+path_data = '/home/icb/anna.chernysheva/McPAS-TCR.csv'
+tcr_repertoire = IRDatasetAdapterFactory("mcpas-tcr")
+
+tcr_repertoire.from_path(path_data)
+
+predictor = TCRSpecificityPredictorFactory("panpep")
+results = predictor.predict(tcr_repertoire, [epitope_1] * len(tcr_repertoire.receptors), repository="/home/icb/anna.chernysheva/PanPep", conda="panpen", pairwise=False)
+print(results.head(3))
