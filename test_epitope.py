11ac8c481ee608250e802377bb6f1d64d673230b
annachernysheva179 <anna.chernysheva@tum.de>
2023-08-17 09:42:37 +0200

    try to add ergo

--- test_epitope.py
+++ test_epitope.py
@@ -21,4 +21,4 @@ tcr_repertoire.from_path(path_data)
 
 predictor = TCRSpecificityPredictorFactory("ergo-I")
 results = predictor.predict(tcr_repertoire, [epitope_1] * len(tcr_repertoire.receptors), repository="/home/icb/anna.chernysheva/ERGO", conda="ergo", pairwise=False)
-print(results.head(3))
+print(results)
