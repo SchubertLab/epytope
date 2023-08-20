0f7f3172355c970f4936ef1addb2e1051501918a
annachernysheva179 <anna.chernysheva@tum.de>
2023-08-20 12:02:22 +0200

    implement comments

--- epytope/TCRSpecificityPrediction/External.py
+++ epytope/TCRSpecificityPrediction/External.py
@@ -472,7 +472,7 @@ class Ergo1(ARepoTCRSpecificityPrediction):
         repository = kwargs["repository"]
         model = "lstm_vdjdb1" if "model" not in kwargs else kwargs["model"]
         model_filepath = os.path.join(repository, "models", f"{model}.pt")
-        return f"ERGO.py predict {model_type} {dataset} {sampling} {cuda} --model_file={model_filepath} --train_data_file=auto --test_data_file={filenames[0]} >> {filenames[1]}"
+        return f"ERGO.py predict {model_type} vdjdb specific {cuda} --model_file={model_filepath} --train_data_file=auto --test_data_file={filenames[0]} >> {filenames[1]}"
 
     def run_exec_cmd(self, cmd, filenames, interpreter=None, conda=None, cmd_prefix=None, repository="", **kwargs):
         super().run_exec_cmd(cmd, filenames, interpreter, conda, cmd_prefix, repository)
