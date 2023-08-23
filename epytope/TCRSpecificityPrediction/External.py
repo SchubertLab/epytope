b94c9f96b496e2c00190c16fb3fbf8b259e4ad45
annachernysheva179 <anna.chernysheva@tum.de>
2023-08-23 17:52:19 +0200

    add DLpTCR

--- epytope/TCRSpecificityPrediction/External.py
+++ epytope/TCRSpecificityPrediction/External.py
@@ -764,6 +764,7 @@ class DLpTCR(ARepoTCRSpecificityPrediction):
             df_tcrs = df_tcrs[(~df_tcrs["TCRA_CDR3"].isna()) & (df_tcrs["TCRA_CDR3"] != "")]
         df_tcrs = df_tcrs[required_columns]
         df_tcrs.drop_duplicates(inplace=True, keep="first")
+        print(df_tcrs)
         return df_tcrs
     
     def save_tmp_files(self, data, **kwargs):
