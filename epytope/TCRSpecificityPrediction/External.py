f83d31dfe97914903c4271d8fd7c00a6db96d51c
annachernysheva179 <anna.chernysheva@tum.de>
2023-08-25 10:49:04 +0200

    debug

--- epytope/TCRSpecificityPrediction/External.py
+++ epytope/TCRSpecificityPrediction/External.py
@@ -721,7 +721,7 @@ class DLpTCR(ARepoTCRSpecificityPrediction):
     __name = "DLpTCR"
     __version = ""
     __trc_length = (8, 20)
-    __epitope_length = (0, 30)
+    __epitope_length = (0, 30) #not sure, 9 for analysis, but no more info
     __repo = "https://github.com/JiangBioLab/DLpTCR"
 
     @property
@@ -770,7 +770,7 @@ class DLpTCR(ARepoTCRSpecificityPrediction):
             df_tcrs = df_tcrs[(~df_tcrs["TCRA_CDR3"].isna()) & (df_tcrs["TCRA_CDR3"] != "")]
             prediction_columns = ["TCRB_CDR3", "TCRA_CDR3", "Epitope"]
         else:
-            raise TypeError(f"Please specify correct model_type: A, B or AB")
+            raise TypeError(f"Please specify a correct model_type: A, B or AB")
         df_tcrs = df_tcrs[required_columns]
         df_tcrs.drop_duplicates(subset=prediction_columns, inplace=True, keep="first")
         df_tcrs = df_tcrs.reset_index(drop=True)
