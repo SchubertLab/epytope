edf33e39755f779e387da7a107d4698e929b6a31
Felix Drost <felix.drost@helmholtz-muenchen.de>
2023-11-02 15:00:41 +0100

    added unit tests

--- epytope/test/external/TestTCRSpecificityPrediction.py
+++ epytope/test/external/TestTCRSpecificityPrediction.py
@@ -77,22 +77,68 @@ class TestTCRSpecificityPrediction(unittest.TestCase):
             for i, epitope in enumerate(epitopes_list):
                 self.assertEqual(results.at[i, ("Epitope", "Peptide")], epitope.peptide, f"{name}: Wrong epitope at position {i}")
                 self.assertEqual(results.at[i, ("Epitope", "MHC")], epitope.allele, f"{name}: Wrong MHC at position {i}")
-    
-
-    def test_tcr_empty_input(self):
-        for name in self.predictors:
-            predictor = TCRSpecificityPredictorFactory(name)
 
     def test_tcr_single_input(self):
         for name in self.predictors:
             predictor = TCRSpecificityPredictorFactory(name)
+            config_predictor = None if name not in self.config_yaml else self.config_yaml[name]
+            results = predictor.predict(self.tcr_repertoire.receptors[:1], self.epitopes[0], pairwise=True, **config_predictor)
+            self.assertEqual(len(results), 1, f"{name}: Length result is different from 1")
+            self.assertIn(self.epitopes[0], results, f"{name}: Epitope not in result")
+            self.assertIn(name, [el.lower() for el in results[self.epitopes[0]].columns.tolist()], f"{name}: Method not in results")
+            self.assertLess(results[self.epitopes[0]].iloc[:, 0].isna().sum(), 1, f"{name}: Method always yield NaN")
+            
+            results = predictor.predict(self.tcr_repertoire.receptors[:1],self.epitopes[0], pairwise=False, **config_predictor)
+            self.assertEqual(len(results), 1, f"{name}: Results have wrong length")
+            self.assertIn(name, [el.lower() for el in results["Method"].columns], f"{name}: Method missing in results")
+            self.assertLess(results["Method"].iloc[:, 0].isna().sum(), 1, f"{name}: Method always yield NaN")
+            self.assertEqual(results.at[0, ("Epitope", "Peptide")], self.epitopes[0].peptide, f"{name}: Wrong epitope at position 0")
+            self.assertEqual(results.at[0, ("Epitope", "MHC")], self.epitopes[0].allele, f"{name}: Wrong MHC at position 0")
+            
+    def form_options(self, config, base_name):
+        base_config = config.copy()
+        options = base_config.pop("options")
+
+        all_configs = {base_name: base_config}
+        for opt_name, opt_dict in options.items():
+            all_configs_new = {}
+            for old_name, old_config in all_configs.items():
+                for add_name, add_value in opt_dict.items():
+                    add_config = {opt_name: add_value}
+                    all_configs_new[f"{old_name}_{add_name}"] = {**old_config, **add_config}
+            all_configs = all_configs_new
+        return all_configs
 
     def test_tcr_options(self):
         if self.options_yaml is None:
             warnings.warn("Skipping Test for predictors options as --options_yaml was not set.")
             return
+        
+        epitopes_pairwise = self.epitopes
+        epitopes_list = self.epitopes * (len(self.tcr_repertoire.receptors)//2)
         for name in self.predictors:
             predictor = TCRSpecificityPredictorFactory(name)
+            if name not in self.options_yaml:
+                continue
+            config_predictor = self.options_yaml[name]
+            configs = self.form_options(config_predictor, name)
+            for opt_name, config in configs.items():
+                results = predictor.predict(self.tcr_repertoire, epitopes_pairwise, pairwise=True, **config_predictor)
+                self.assertEqual(len(results), len(self.tcr_repertoire.receptors), "Results have wrong length")
+                for epitope in epitopes_pairwise:
+                    self.assertIn(epitope, results, f"{opt_name}: Epitope not in result")
+                    self.assertIn(name, [el.lower() for el in results[epitope].columns.tolist()],
+                                  f"{opt_name}: Method not in results")
+                    self.assertLess(results[epitope].iloc[:, 0].isna().sum(), len(results),
+                                    f"{opt_name}: Method always yield NaN")
+                
+                results = predictor.predict(self.tcr_repertoire, epitopes_list, pairwise=False, **config_predictor)
+                self.assertEqual(len(results), len(self.tcr_repertoire.receptors), f"{opt_name}: Results have wrong length")
+                self.assertIn(name, [el.lower() for el in results["Method"].columns], f"{opt_name}: Method missing in results")
+                self.assertLess(results["Method"].iloc[:, 0].isna().sum(), len(results), f"{opt_name}: Method always yield NaN")
+                for i, epitope in enumerate(epitopes_list):
+                    self.assertEqual(results.at[i, ("Epitope", "Peptide")], epitope.peptide, f"{opt_name}: Wrong epitope at position {i}")
+                    self.assertEqual(results.at[i, ("Epitope", "MHC")], epitope.allele, f"{opt_name}: Wrong MHC at position {i}")
 
 
 if __name__ == "__main__":
