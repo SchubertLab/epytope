f9b3ceebac4acb6983db9cab14bf66aa9adb1ea7
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2023-11-14 23:03:47 +0100

    Create test_tcellmatch.py

---
+++ test_tcellmatch.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('/epytope')
+
+from epytope.Core import Peptide, Allele
+from epytope.Core import Peptide, Allele, TCREpitope, ImmuneReceptorChain, ImmuneReceptor
+from epytope.IO import IRDatasetAdapterFactory
+from epytope.TCRSpecificityPrediction import TCRSpecificityPredictorFactory
+import pandas as pd
+
+
+peptide = Peptide("SYFPEITHI")
+allele = Allele("HLA-A*02:01")
+epitope_1 = TCREpitope(peptide=peptide, allele=allele)
+
+epitope_2 = TCREpitope(peptide="EAAGIGILT", allele=None)
+
+path_data = '/files/vdjbd.tsv'
+tcr_repertoire = IRDatasetAdapterFactory("vdjdb")
+
+tcr_repertoire.from_path(path_data)
+tcr_repertoire.receptors = tcr_repertoire.receptors[:100]
+
+
+
+predictor = TCRSpecificityPredictorFactory("tcellmatch")
+results = predictor.predict(tcr_repertoire, [epitope_1] * len(tcr_repertoire.receptors), repository="/tcellmatch", conda="ergo", pairwise=False)
+print(results)
