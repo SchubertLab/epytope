93e8c6fd5435021fc808fcfe892f4dac02fed364
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2023-11-30 15:46:17 +0100

    Update download.sh

--- external/download.sh
+++ external/download.sh
@@ -81,7 +81,8 @@ conda create --name epytope_stapler python=3.9
 cd /external/tcellmatch
 conda run -n epytope_numpy195 pip install -e .
 conda run -n epytope_torch11 conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
-conda run -n deepTCR conda uninstall tensorflow-gpu -y && conda install tensorflow==2.1.0
+conda run -n deepTCR conda uninstall tensorflow-gpu -y
+conda run -n deepTCR conda install tensorflow==2.1.0
 cd /external/STAPLER
 conda run -n epytope_stapler pip install stitchr IMGTgeneDL
 conda run -n epytope_stapler pip install -e .
