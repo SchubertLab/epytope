4aa9144c00d3002955ceb8d29d7d049208e43129
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2023-11-30 07:58:46 +0100

    Update download.sh

--- external/download.sh
+++ external/download.sh
@@ -77,5 +77,16 @@ conda env create -f /epytope/external/ergo.yml
 conda env create -f /epytope/external/atm.yml
 conda env create -f /epytope/external/tcellmatch.yml
 conda env create -f /epytope/external/titan.yml
+conda create --name epytope_stapler python=3.9
 cd /external/tcellmatch
 conda run -n epytope_numpy195 pip install -e .
+conda run -n epytope_torch11 conda install pytorch==1.10.1 torchvision==0.11.2 torchaudio==0.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+conda run -n deepTCR conda uninstall tensorflow-gpu -y && conda install tensorflow==2.1.0
+/external/STAPLER
+conda run -n epytope_stapler pip install stitchr IMGTgeneDL
+conda run -n epytope_stapler pip install -e .
+conda run -n epytope_stapler pip install torch==2.1.0 torchaudio==2.1.0 torchvision==0.16
+conda run -n epytope_stapler stitchrdl -s human
+conda run -n epytope_stapler pip install x-transformers==0.22.3
+cd /epytope
+conda run -n epytope pip install -e .
