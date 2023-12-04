b31cdf08092bb097dde64bfd6d0a25266c4cfc23
annachernysheva179 <131196587+annachernysheva179@users.noreply.github.com>
2023-12-04 18:59:25 +0100

    Create create_envs.sh

---
+++ external/create_envs.sh
@@ -0,0 +1,7 @@
+conda env create -f /epytope/external/environment.yml
+conda env create -f /external/ImRex/environment.yml
+conda env create -f /epytope/external/ergo.yml
+conda env create -f /epytope/external/atm.yml
+conda env create -f /epytope/external/tcellmatch.yml
+conda env create -f /epytope/external/titan.yml
+conda create --name epytope_stapler python=3.9
