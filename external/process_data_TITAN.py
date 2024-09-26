3ae081a34954276dcb064a2358a4e76f86a7e040
Ubuntu <ubuntu@gentcr-6f2ae.openstack.bielefeld>
2024-09-26 12:22:23 +0000

    remove docker for benchmark

--- external/process_data_TITAN.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-fasta = []
-with open(sys.argv[1], "r") as f:
-    fasta.extend(f.readlines())
-del fasta[:67]
-del fasta[-47:]
-with open(sys.argv[2], "w") as f:
-    f.writelines(fasta)
